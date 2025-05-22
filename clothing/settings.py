b9d9a22236d51b2bf8b460b5518e47873a5e74e9
estellespecht <estellespecht@hotmail.co.uk>
2025-05-22 16:08:26 +0200

    Add Crispy Forms, Crispy Tailwind and Widget Tweaks to style forms

--- clothing/settings.py
+++ clothing/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_tailwind',
+    'widget_tweaks',
     'marketplace',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
